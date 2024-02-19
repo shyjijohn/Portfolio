9836a31052a2bb2a961576c1df563741aa1b3269
Shyji John <shyjijohn90@gmail.com>
2024-02-19 22:10:09 +0000

    sample done

---
+++ resume/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div class="left-container">
+        <div class = "hello">HELLO</div>
+        <div class = "me">I'm <span>SHYJI JOHN</span></div>
+        <div class = "aspiring">Aspiring</div>
+        <div class = "developer">Frontend developer</div>
+      </div>
+      <div class="right-container">
+        <div class="picture">
+          <div><img src = "images/resume.png" class = "me" /></div>
+          <div class = "next-page">
+            <div class = "first-dot"></div>
+            <div class = "second-dot"></div>
+            <div class = "third-dot"></div>
+            <div class = "fourth-dot"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
