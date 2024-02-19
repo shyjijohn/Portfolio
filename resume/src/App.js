663a67b2300a01736f28ff6b4bdeea1c187b70ab
Shyji John <shyjijohn90@gmail.com>
2024-02-19 23:55:39 +0000

    css changes

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,24 +4,34 @@ function App() {
   return (
     <div class="container">
       <div class="left-container">
-        <div class = "hello">HELLO</div>
-        <div class = "me">I'm <span>SHYJI JOHN</span></div>
-        <div class = "aspiring">Aspiring</div>
-        <div class = "developer">Frontend developer</div>
+        <div class="hello">HELLO</div>
+        <div class="me">I'm <span>SHYJI JOHN</span></div>
+        <div class="role">
+          <div class="aspiring">Aspiring</div>
+          <div class="developer">Frontend developer</div>
+        </div>
       </div>
       <div class="right-container">
         <div class="picture">
-          <div><img src = "images/resume.png" class = "me" /></div>
-          <div class = "next-page">
-            <div class = "first-dot"></div>
-            <div class = "second-dot"></div>
-            <div class = "third-dot"></div>
-            <div class = "fourth-dot"></div>
-          </div>
+          <img src="images/resume.png" class="imageMe" />
         </div>
+        <div class = "next-page">
+
+          </div>
+        {/* <div class="picture">
+          <img src="images/resume.png" class="imageMe" />
+         
+        </div> */}
       </div>
     </div>
   );
 }
 
 export default App;
+
+{/* <div class = "next-page">
+            <div class = "first-dot"></div>
+            <div class = "second-dot"></div>
+            <div class = "third-dot"></div>
+            <div class = "fourth-dot"></div>
+          </div> */}
