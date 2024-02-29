97af8b8bb9ec0a35dc47fdbc29f1dc492d46ce41
Shyji John <shyjijohn90@gmail.com>
2024-02-29 11:14:50 +0000

    made ui adjustable to all screens

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,34 +1,67 @@
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     // Your scroll event handler logic here
+  //     console.log('Window scrolled!');
+  //   };
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   // Clean up the event listener on unmount
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []); // Empty dependency array means this effect runs only once, similar to componentDidMount and componentWillUnmount
+
+
+
+
+
+
   return (
-    <div class="container">
-      <div class="left-container">
-        <div class="hello">HELLO</div>
-        <div class="me">I'm <span>SHYJI JOHN</span></div>
-        <div class="role">
-          <div class="aspiring">Aspiring</div>
-          <div class="developer">Frontend developer</div>
-        </div>
-      </div>
-      <div class="right-container">
-        <div class="picture">
-          <img src="images/resume.png" class="imageMe" />
-        </div>
-        <div class = "next-page">
-    `     <div class = "next-page">
-            <button class = "first-dot"></button>
-            <button class = "second-dot"></button>
-            <button class = "third-dot"></button>
-            <button class = "fourth-dot"></button>
-          </div> `
+    <div class="app">
+      <div class="container">
+        <div class="firstPage">
+          <div class="left-container">
+            <div class="hello">HELLO</div>
+            <div class="me">I'm <span>SHYJI JOHN</span></div>
+            <div class="role">
+              <div class="aspiring">Aspiring</div>
+              <div class="developer">Frontend developer</div>
+            </div>
           </div>
-       
+          <div class="right-container">
+            <div class="picture">
+              <img src="images/resume.png" class="imageMe" />
+            </div>
+          </div>
+        </div>
       </div>
+      {/* <div class="container">
+        <div class="firstPage">
+          <div class="left-container">
+            <div class="hello">HELLO</div>
+            <div class="me">I'm <span>SHYJI JOHN</span></div>
+            <div class="role">
+              <div class="aspiring">Aspiring</div>
+              <div class="developer">Frontend developer</div>
+            </div>
+          </div>
+          <div class="right-container">
+            <div class="picture">
+              <img src="images/resume.png" class="imageMe" />
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
 
 export default App;
-
- 
