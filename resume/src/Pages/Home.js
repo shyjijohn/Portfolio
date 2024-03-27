c6ba7ade2a103caa35324db51f21c7e245834f41
Shyji John <shyjijohn90@gmail.com>
2024-03-27 19:59:29 +0000

    Responsive with out overlap

--- resume/src/Pages/Home.js
+++ resume/src/Pages/Home.js
@@ -3,15 +3,25 @@
 import React from 'react'
 import Container from '../Layout/Container'
 import Intro from '../Intro'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Home() {
 
     return (
         <Container>
-            <Intro/>
-            <div className=''>
-                <img src="images/resume.png" className='md:-ml-20 lg:-mt-20 lg:min-w-96 object-contain md:w-fit md:h-full' />
+            <Intro />
+            <div className=' flex flex-col h-full w-full justify-between overflow-hidden'>
+                <div className=' self-start w-full h-5/6 flex flex-col justify-center'>
+                    <img src="images/resume.png" className='lg:min-w-96 object-cover max-h-full' />
+                </div>
+                <div className='self-end pr-20 pb-5'>
+                    <GitHubIcon className='mr-3' />
+                    <LinkedInIcon />
+                </div>
             </div>
+
+
         </Container>
     )
 }
