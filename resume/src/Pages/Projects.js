a77ca84eb2a947073383c5ef396d35eb03bed668
Shyji John <shyjijohn90@gmail.com>
2024-03-28 17:27:09 +0000

    1 Project included

--- resume/src/Pages/Projects.js
+++ resume/src/Pages/Projects.js
@@ -8,10 +8,10 @@ function Project({ imageUrl, githubUrl, liveDemoUrl }) {
 
     return (
         <div className=' bg-white w-56 h-72 rounded-2xl flex flex-col items-center justify-center hover:border-4  m-3'>
-            <div className='bg-blue-300 m-3 self-stretch h-56'>
-                <img className='w-full' src={imageUrl} />
+            <div className='bg-slate-200 rounded-2xl mx-3 m-1 self-stretch h-56'>
+                <img className='w-full object-contain h-full p-1' src={imageUrl} />
             </div>
-            <div className='flex flex-row mb-2'>
+            <div className='flex flex-row mb-1'>
 
                 {githubUrl != null && (
                     <>
@@ -46,7 +46,7 @@ export default function Projects() {
             <div className='w-full h-full flex flex-col justify-start items-center mr-10 ml-10'>
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 mt-3 md:mt-10">Projects</h2>
                 <div className=' w-full h-full flex flex-wrap justify-evenly pt-4 overflow-auto' >
-                    <Project />
+                    <Project imageUrl={"images/RateGrapher.png"} githubUrl={"https://github.com/shyjijohn/Currency-Converter"} liveDemoUrl={"https://shyjijohn.github.io/Currency-Converter/"} />
                     <Project />
                     <Project />
                     <Project />
