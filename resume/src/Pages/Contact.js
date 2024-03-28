a3cdcec8dd4ba6c58f6362060714bf241cc05791
Shyji John <shyjijohn90@gmail.com>
2024-03-28 15:37:53 +0000

    contact page mobile landscape working

--- resume/src/Pages/Contact.js
+++ resume/src/Pages/Contact.js
@@ -11,9 +11,9 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 export default function Contact() {
     return (
         <Container>
-            <div className=' self-stretch flex flex-col justify-between'>
-                <div className=' flex flex-col justify-center items-center self-start p-5 md:pt-40'>
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 mt-3 md:mt-10">Contact me</h2>
+            <div className=' h-full flex flex-col justify-between'>
+                <div className=' flex-grow flex flex-col justify-center items-center p-5 self-center '>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 mt-3 ">Contact me</h2>
                     <h2 className="text-xl font-normal tracking-tight text-gray-900 mt-5 ">Let's build something amazing together!</h2>
                     <h2 className="text-2xl font-normal tracking-tight text-gray-900 ">Reach out to discuss your next project or just to say hi</h2>
                     <div className='bg-white border-2 rounded-2xl p-5 mt-10 flex flex-col items-start justify-start'>
